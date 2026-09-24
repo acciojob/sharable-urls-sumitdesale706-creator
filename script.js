c45527a281b7f19d118a027693193117b11a9a17
Sumit Desale <sumitdesale706@gmail.com>
2026-09-25 01:00:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+const form = document.querySelector("form");
+        const url = document.getElementById("url");
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+
+            let query = [];
+
+            if (name) {
+                query.push(`name=${name}`);
+            }
+
+            if (year) {
+                query.push(`year=${year}`);
+            }
+
+            if (query.length > 0) {
+                url.textContent = `https://localhost:8080/?${query.join("&")}`;
+            } else {
+                url.textContent = "https://localhost:8080/";
+            }
+        });
